74931d5a887ee219d164b4b26825f3c603557d55
arnasRad <arnasrad@gmail.com>
2019-08-21 16:13:54 +0300

    End of video 67 - Field Injection

--- Section8_Annotations-Dependency-Injection/src/com/arnasRad/dependencyinjection/TennisCoach.java
+++ Section8_Annotations-Dependency-Injection/src/com/arnasRad/dependencyinjection/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+    @Autowired
     private FortuneService fortuneService;
 
     // define a default constructor
@@ -22,12 +23,14 @@ public class TennisCoach implements Coach {
     }
     */
 
+    /*
     // inject a method
     @Autowired
     public void doSomeCrazyStuff(FortuneService fortuneService) {
         System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
         this.fortuneService = fortuneService;
     }
+    */
 
     /*
     @Autowired
