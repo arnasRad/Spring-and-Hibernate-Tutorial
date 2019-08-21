74a8d0f0eeb04964c89d0f951e0333dcc8c53787
arnasRad <arnasrad@gmail.com>
2019-08-21 11:17:09 +0300

    End of video 61 - Constructor Injection with Spring Annotations

---
+++ Section8_Annotations-Dependency-Injection/src/com/arnasRad/dependencyinjection/Coach.java
@@ -0,0 +1,8 @@
+package com.arnasRad.dependencyinjection;
+
+public interface Coach {
+
+    public String getDailyWorkout();
+
+    public String getDailyFortune();
+}
---
+++ Section8_Annotations-Dependency-Injection/src/com/arnasRad/dependencyinjection/FortuneService.java
@@ -0,0 +1,6 @@
+package com.arnasRad.dependencyinjection;
+
+public interface FortuneService {
+
+    public String getFortune();
+}
